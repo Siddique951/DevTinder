0be28a73324d37f137209997952e36366e875882
Shaikh Siddique <sheikhsiddique9630@gmail.com>
2025-12-19 04:53:19 -0800

    Middleware

--- src/app.js
+++ src/app.js
@@ -1,28 +1,30 @@
 const express = require("express");
-
 const app = express();
 
-// This will only handle Get call to /user
-app.get('/user', (req, res) => {
-    res.send({ firstname: "Siddique", lastname: "Shaikh" })
-})
-
-// This will only handle Post /user
-app.post('/user', (req, res) => {
-    // saving data to DB 
-    res.send("Data Successfully Saved to the Data base")
-})
-
-
-app.delete('/user', (req, res) => {
-    res.send("Deleted Successfully")
-})
-
-// This will match all the HTTP method API calls to /test
-app.use('/test', (req, res) => {
-    res.send("Hello Form The Server");
-})
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user! (1)");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2! (2)");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 3! (3)");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4! (4)");
+    next();
+  },
+  (req, res) => {
+    console.log("Handling the route user 5! (final)");
+    res.send("5th Response!");
+  }
+);
 
 app.listen(7777, () => {
-    console.log("server is running on port 7777");
+  console.log("server is running on port 7777");
 });
